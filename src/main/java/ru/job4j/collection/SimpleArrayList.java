bfc97af5df88d52f15b74e07a87434d3b427179e
Windows <ivinjc@gmail.com>
2022-05-24 09:33:43 +0300

    Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,89 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        modCount++;
+        if (size == container.length) {
+            arrayGrowth();
+        }
+        container[size++] = value;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        modCount++;
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        modCount++;
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        System.arraycopy(
+                container,
+                index + 1,
+                container,
+                index,
+                container.length - index - 1
+        );
+        container[container.length - 1] = null;
+        size--;
+        return oldValue;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int expectedModCount = modCount;
+            private int cursorPoint;
+            @Override
+            public boolean hasNext() {
+                return cursorPoint < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[cursorPoint++];
+            }
+        };
+    }
+
+    private void arrayGrowth() {
+        container = Arrays.copyOf(
+                container, container.length * 2
+        );
+    }
+}
