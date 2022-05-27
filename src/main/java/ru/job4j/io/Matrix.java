ea01b2173b2121180e0f3eea027596f0197a50f0
Windows <ivinjc@gmail.com>
2022-05-27 18:07:41 +0300

    FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Matrix {
+    public static void multiple(int size) {
+        int[][] matrix = new int[size][size];
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    matrix[i][j] = (i + 1) * (j + 1);
+                    out.write(String.valueOf(matrix[i][j]).getBytes());
+                    out.write("   ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        multiple(9);
+    }
+}
