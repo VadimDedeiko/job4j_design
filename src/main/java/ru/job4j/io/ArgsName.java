b3ed1d7d7d35eef8edbc6c9bb15c8f33c39c9fc6
Windows <ivinjc@gmail.com>
2022-05-29 19:14:30 +0300

    Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        String rsl = values.get(key);
+        if (rsl == null) {
+            throw new IllegalArgumentException("Argument not passed: -" + key);
+        }
+        return rsl;
+    }
+
+    private void parse(String[] args) {
+        String equal = null;
+        for (String arg : args) {
+            if (arg.endsWith("=")) {
+                equal = "=";
+            }
+            String[] line = arg.split("=");
+            if (line.length > 2) {
+                for (int i = 2; i < line.length; i++) {
+                    line[1] += "=" + line[i];
+                    line[1] += equal;
+                }
+            }
+            String[] newLine = Arrays.copyOf(line, 2);
+            if (newLine.length != 2) {
+                throw new IllegalArgumentException(
+                        "Pass the argument as key=value (" + arg + ")"
+                );
+            }
+            values.put(newLine[0].replaceFirst("-", ""), newLine[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
