721b231c90b0cd82e1c856152e30bfd60d25ce56
Windows <ivinjc@gmail.com>
2022-06-04 20:20:44 +0300

    Select [#6861 #308759]

--- data/fauna.sql
+++ data/fauna.sql
@@ -12,4 +12,4 @@ insert into fauna(name, avg_age, discovery_date) values ('seaweed', 23000, '1938
 select * from fauna where name like '%fish%';
 select * from fauna where avg_age > 10000 and avg_age < 21000;
 select * from fauna where discovery_date is null;
-select discovery_date < '1950-01-01';
+select * from fauna where discovery_date < '1950-01-01';
