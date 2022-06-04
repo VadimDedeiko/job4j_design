f2763d9c91b2272fc474f98017af12daa4673a0d
Windows <ivinjc@gmail.com>
2022-06-04 11:00:09 +0300

    Группировка и агрегатные функции [#373618]

--- data/group.sql
+++ data/group.sql
@@ -35,19 +35,13 @@ select avg(price) from devices;
 
 select p.name, avg(d.price)
 from devices_people as dp
-inner join
-devices as d
-inner join
-people as p
-on dp.people_id = d.id
+inner join devices as d on dp.device_id = d.id
+inner join people as p on dp.people_id = p.id 
 group by p.name;
 
 select p.name, avg(d.price)
 from devices_people as dp
-inner join
-devices as d
-inner join
-people as p
-on dp.people_id = d.id
+inner join devices as d on dp.device_id = d.id
+inner join people as p on dp.people_id = p.id 
 group by p.name
 having avg(d.price) > 5000;
