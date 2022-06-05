78d2ea1afde7e56db76c974d00fb0f40a3413187
Windows <ivinjc@gmail.com>
2022-06-05 13:37:20 +0300

    Хранилище машин [#1733 #308947]

--- data/cardetails.sql
+++ data/cardetails.sql
@@ -1,35 +1,49 @@
-create table car(
+create table body(
 id serial primary key,
 name varchar(50)
 );
 
-create table details(
+create table motor(
 id serial primary key,
-body bool,
-motor bool,
-transmission bool,
-car_id int references car(id)
+name varchar(50)
 );
 
-insert into car (name) values ('car1');
-insert into car (name) values ('car2');
-insert into car (name) values ('car3');
-insert into car (name) values ('car4');
+create table transmission(
+id serial primary key,
+name varchar(50)
+);
 
-insert into details (body, motor, transmission, car_id) values (true,true,true,1);
-insert into details (body, motor, transmission, car_id) values (true,true,true,2);
-insert into details (body, motor, transmission, car_id) values (true,true,true,3);
-insert into details (body, motor, transmission, car_id) values (true,true,true,4);
-insert into details (body, motor, transmission, car_id) values (true,true,null,1);
-insert into details (body, motor, transmission, car_id) values (true,null,true,1);
 
-select * from details as d left outer join car as c on d.car_id = c.id;
+create table car(
+id serial primary key,
+name varchar(50),
+body_id int references body(id),
+motor_id int references motor(id),
+transmission_id int references transmission(id)
+);
 
-select * from details as d 
-left outer join car as c on d.car_id = c.id where d.body is null;
-select * from details as d 
-left outer join car as c on d.car_id = c.id where d.motor is null;
-select * from details as d 
-left outer join car as c on d.car_id = c.id where d.transmission is null;
-select * from details as d 
-left outer join car as c on d.car_id = c.id where d.car_id is null;
+insert into body (name)
+ values ('body1'),('body2'), ('body3');
+insert into motor (name)
+ values ('motor1'), ('motor2'), ('motor3');
+insert into transmission (name)
+values ('transmission1'), ('transmission2'), ('transmission3');
+
+insert into car (name, body_id, motor_id, transmission_id)
+values ('car1', 1, 1, 1), ('car2', 2, 2, 2), ('car3', 1, 1, null);
+
+--¬ывести список всех машин и все прив€занные к ним детали.
+select k.name as c, v.name as b, motor.name as e, transmission.name as t
+from
+car as k left join body as v on k.body_id = v.id
+left join motor on k.motor_id = motor.id
+left join transmission on k.transmission_id = transmission.id;
+
+--¬ывести отдельно детали, которые не используютс€ в машине, кузова, двигатели, коробки передач.
+select body.name from
+body left join car on body.id = car.body_id where car.id is null
+union
+select motor.name from motor left join car on motor.id = car.body_id where car.id is null
+union
+select transmission.name from transmission left join car on transmission.id = car.body_id
+where car.id is null;
