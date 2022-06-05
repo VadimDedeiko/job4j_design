46ef6374b81fea57fad2ba6e0601a150b1bbf230
Windows <ivinjc@gmail.com>
2022-06-05 10:57:57 +0300

    Фильтры [#1732 #308903]

--- data/filtres.sql
+++ data/filtres.sql
@@ -22,21 +22,24 @@ insert into product (name, type_id, expired_date, price) values ('Сыр', 1, '2
 insert into product (name, type_id, expired_date, price) values ('Сыр плавленный', 1, '2022-06-07', 30);
 insert into product (name, type_id, expired_date, price) values ('мороженое', 2, '2022-06-07', 30);
 insert into product (name, type_id, expired_date, price) values ('Торт', 2, '2022-06-07', 300);
+insert into product (name, type_id, expired_date, price) values ('Мука', 3, '2022-06-07', 300);
 insert into product (name, type_id, expired_date, price) values ('Хлеб', 3, '2022-06-07', 40);
 
-select * from product where name = 'Сыр';
+select * from tipen where name = 'Сыр';
 select * from product where name like '%мороженое%';
-select * from product where expired_date > current_date;
+select * from product where expired_date < current_date;
 
-select name, max(price) from product;
+select name, price
+from product
+where price=(select MAX(price) FROM product);
 
-
-select count(name) as Количество
-from tipen inner join product as p on p.type_id = tipen.id;
+select t.name as Продукт, count(p.name) as Количество
+from tipen as t inner join product as p on p.type_id = t.id
+group by t.name;
 
 select t.name, p.name
 from tipen as t inner join product as p on p.type_id = t.id
-where t.name = 'Сыр' and t.name = 'Молоко';
+where t.name = 'Сыр' or t.name = 'Молоко';
 
 select t.name as Тип_продукта, count(t.name) as Количество
 from product as p inner join tipen as t on p.type_id = t.id
