0380be5c7af3566c3ce6d6088bb936cf3522eda4
Windows <ivinjc@gmail.com>
2022-06-26 18:15:05 +0300

    Парковка машин [#853 #316264]

--- src/test/java/ru/job4j/ood/carparking/ParksTest.java
+++ src/test/java/ru/job4j/ood/carparking/ParksTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.ood.carparking;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
 
 public class ParksTest {
+    @Ignore
     @Test
     public void whenAllCars() {
         Passenger passenger1 = new Passenger();
@@ -18,6 +20,7 @@ public class ParksTest {
         Assert.assertEquals(List.of(passenger1, passenger2, track), parks.getCarList());
     }
 
+    @Ignore
     @Test
     public void whenTwoTrack() {
         Track track1 = new Track(2);
@@ -28,7 +31,7 @@ public class ParksTest {
         parks.add(track2);
         Assert.assertEquals(List.of(track1, track2), parks.getCarList());
     }
-
+    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void whenException() {
         Track track = new Track(1);
