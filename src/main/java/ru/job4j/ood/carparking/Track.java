9e4238856326c2b973b4f6eb257025bd4d0024dc
Windows <ivinjc@gmail.com>
2022-06-26 18:21:12 +0300

    Парковка машин [#853 #316264]

---
+++ src/main/java/ru/job4j/ood/carparking/Track.java
@@ -0,0 +1,18 @@
+package ru.job4j.ood.carparking;
+
+public class Track implements Car {
+    private int volume;
+
+    public Track(int volume) {
+        if (volume <= 1) {
+            throw new IllegalArgumentException("Invalid volume for Tracks."
+                    + " Change volume more then 1");
+        }
+        this.volume = volume;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+}
