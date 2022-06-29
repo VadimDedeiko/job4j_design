1b5a0d789ced0dbfa5bd920b8837c4370033771c
Windows <ivinjc@gmail.com>
2022-06-29 12:25:47 +0300

    Создать меню. [#4748 #316830]

--- src/main/java/ru/job4j/ood/isp/menu/Menu.java
+++ src/main/java/ru/job4j/ood/isp/menu/Menu.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 public interface Menu extends Iterable<Menu.MenuItemInfo> {
 
     String ROOT = null; /** Константа, указывающая, что нужно добавить элемент в корень */
-
     boolean add(String parentName, String childName, ActionDelegate actionDelegate);
 
     Optional<MenuItemInfo> select(String itemName);
