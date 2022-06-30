dfd0096fe2579807b3c748d49dd7ef6992ba8a54
Windows <ivinjc@gmail.com>
2022-06-30 18:17:11 +0300

    Создать меню. [#4748 #316830]

--- src/test/java/ru/job4j/ood/isp/menu/SimpleMenuTest.java
+++ src/test/java/ru/job4j/ood/isp/menu/SimpleMenuTest.java
@@ -70,8 +70,7 @@ public class SimpleMenuTest {
         menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
         menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
         menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
-        String expected =
-                """
+        String expected = """
                 1.Сходить в магазин
                 ----1.1.Купить продукты
                 --------1.1.1.Купить хлеб
