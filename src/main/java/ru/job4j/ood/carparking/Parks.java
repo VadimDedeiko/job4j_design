adf77750f6adb73f5110e09b33ab1dee4a98251b
Windows <ivinjc@gmail.com>
2022-07-02 12:25:13 +0300

    Парковка машин [#853 #316264]

--- src/main/java/ru/job4j/ood/carparking/Parks.java
+++ src/main/java/ru/job4j/ood/carparking/Parks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parks implements Park {
+
     private int placePassenger;
     private int placeTrack;
     private List<Car> carList;
