ea297f7578db5e4df5ecda6cc4d7e3f0907cdafc
Windows <ivinjc@gmail.com>
2022-07-03 12:05:14 +0300

    Парковка машин [#853 #316264]

--- src/main/java/ru/job4j/ood/carparking/Parks.java
+++ src/main/java/ru/job4j/ood/carparking/Parks.java
@@ -23,7 +23,7 @@ public class Parks implements Park {
     @Override
     public boolean add(Car car) {
         boolean rsl = false;
-        if (car.getVolume() == Passenger.SIZE && placePassenger > 0) {
+        if (car.getVolume() == Passenger.SIZE && placePassenger >= Passenger.SIZE) {
             carList.add(car);
             placePassenger--;
             rsl = true;
@@ -31,7 +31,7 @@ public class Parks implements Park {
             carList.add(car);
             placeTrack--;
             rsl = true;
-        } else if (car.getVolume() > Passenger.SIZE && placeTrack == 0
+        } else if (car.getVolume() > Passenger.SIZE && placeTrack <  Passenger.SIZE
                 && !(placePassenger < car.getVolume())) {
             carList.add(car);
             placePassenger -= car.getVolume();
