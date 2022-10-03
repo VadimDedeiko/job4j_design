4c47e920b232bab05c820f21c9fa7f454f284598
Windows <ivinjc@gmail.com>
2022-10-03 17:49:05 +0300

    Нормализация БД [#504822 #347035]

--- db/scripts/database_normalization.sql
+++ db/scripts/database_normalization.sql
@@ -1,14 +1,15 @@
-CREATE TABLE person
+CREATE TABLE gender
 (
-    id     SERIAL PRIMARY KEY,
-    name   varchar(255),
-    gender varchar(8)
+    id  SERIAL PRIMARY KEY,
+    sex varchar(25)
 );
 
-CREATE TABLE address
+CREATE TABLE person
 (
-    id      SERIAL PRIMARY KEY,
-    address text
+    id        SERIAL PRIMARY KEY,
+    name      varchar(255),
+    address   varchar(500),
+    gender_id int references gender (id)
 );
 
 CREATE TABLE movies
@@ -21,18 +22,15 @@ CREATE TABLE delivery
 (
     id         SERIAL PRIMARY KEY,
     movies_id  integer references movies (id),
-    person_id  integer references person (id),
-    address_id integer references address (id)
+    person_id  integer references person (id)
 );
 
-INSERT INTO person (name, gender)
-VALUES ('Ольга Егорова', 'женский'),
-       ('Иванов Сергей', 'мужской');
+INSERT INTO gender (sex) values ('Мужской'), ('Женский');
 
-INSERT INTO address (address)
-VALUES ('1-й Казанский перулок, д. 14'),
-       ('ул. Центральная, д. 40, кв. 74'),
-       ('ул. Ленина, д. 7, кв. 130');
+INSERT INTO person (name, address, gender_id)
+VALUES ('Ольга Егорова', '1-й Казанский перулок, д. 14', 2),
+       ('Иванов Сергей', 'ул. Центральная, д. 40, кв. 74', 1),
+       ('Иванов Сергей', 'ул. Ленина, д. 7, кв. 130', 1);
 
 INSERT INTO movies (name)
 VALUES ('Пираты Карибского моря'),
@@ -40,12 +38,12 @@ VALUES ('Пираты Карибского моря'),
        ('Человек, который изменил все'),
        ('Интерстеллар');
 
-INSERT INTO delivery (movies_id, person_id, address_id)
-VALUES (1, 1, 1),
-       (2, 1, 1),
-       (3, 2, 2),
-       (4, 2, 2),
-       (2, 2, 3);
+INSERT INTO delivery (person_id, movies_id)
+VALUES (1, 1),
+       (1, 2),
+       (2, 3),
+       (2, 4),
+       (3, 2);
 
 SELECT * from delivery;
 
